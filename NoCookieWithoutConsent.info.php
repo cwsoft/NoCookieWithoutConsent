bda41070dd095ed4195ca021415a7d30445ce1b9
cwsoft <info@cwsoft.de>
2023-08-17 20:31:22 +0200

    Set version to 1.0.1

--- NoCookieWithoutConsent.info.php
+++ NoCookieWithoutConsent.info.php
@@ -10,7 +10,7 @@ $info = array(
     'title' => 'NoCoWoCo - No Cookie Without Consent',
     'summary' => 'Disables wire frontend cookies until user consent to use required cookies.',
     'icon' => 'cookie',
-    'version' => '1.0.0',
+    'version' => '1.0.1',
     'author' => 'cwsoft',
     'href' => 'https://github.com/cwsoft/NoCookieWithoutConsent',
     'requires' => 'PHP>=8.1',
